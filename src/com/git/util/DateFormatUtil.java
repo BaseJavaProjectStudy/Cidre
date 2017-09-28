c8d113245fad1fd7efcdf0e683eba20efb29b745
L <L@WZB>
2017-09-29 00:03:33 +0800

    commit Date util code

--- src/com/git/util/DateFormatUtil.java
+++ src/com/git/util/DateFormatUtil.java
@@ -1,10 +1,26 @@
 package com.git.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * 日期类型工具类
  * @author andy
  *
  */
 public class DateFormatUtil {
-
+public static  String getDate(){
+	Date date = new Date();
+	DateFormat dateInstance = SimpleDateFormat.getDateInstance();
+	String format = dateInstance.format(date);
+	return format;
+	
+}
+public static void main(String[] args) {
+	String date = getDate();
+	System.out.println(date);
+	
+}
 }
